96d525cb8e2a28cd63a61252bea598d7c0aa140b
Thierry Escande <tescande@gmail.com>
2024-12-22 18:20:07 +0100

    Day 10 part 2

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,5 +1,6 @@
 #!/bin/env python
-# Solution is 574
+# Solution part 1 is 574
+# Solution part 2 is 1238
 
 import sys
 
@@ -24,17 +25,18 @@ def get_at(matrix, r, c):
 	except:
 		return None
 
-def walk_trail(matrix, r, c):
+def walk_trail(matrix, r, c, part1):
 	v = get_at(matrix, r, c)
 	if v == 9:
-		matrix[r][c] = -1
+		if part1:
+			matrix[r][c] = -1
 		return 1
 
 	res = 0
 	for m in direction:
 		next_v = get_at(matrix, r + m[0], c + m[1])
 		if next_v is not None and next_v == v + 1:
-			res += walk_trail(matrix, r + m[0], c + m[1])
+			res += walk_trail(matrix, r + m[0], c + m[1], part1)
 
 	return res
 
@@ -51,14 +53,17 @@ def main():
 		for line in f:
 			matrix.append(list(map(int, line[:-1])))
 
-	res = 0
+	res_p1 = 0
+	res_p2 = 0
 	for r in range(len(matrix)):
 		for c in range(len(matrix[r])):
 			if get_at(matrix, r, c) == 0:
-				res += walk_trail(matrix, r, c)
+				res_p1 += walk_trail(matrix, r, c, True)
 				restore_matrix(matrix)
+				res_p2 += walk_trail(matrix, r, c, False)
 
-	print(f"res: {res}")
+	print(f"Part 1: {res_p1}")
+	print(f"Part 2: {res_p2}")
 
 if __name__ == "__main__":
     exit(main())
