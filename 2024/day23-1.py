481b385414ca7f37ec398aa0d0b3775109069ad8
Thierry Escande <tescande@gmail.com>
2025-01-05 16:56:06 +0100

    Day 23 part 1

---
+++ 2024/day23-1.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+# Solution is 1156
+
+from collections import defaultdict
+import sys
+
+def main():
+	servers = defaultdict(set)
+
+	file = sys.argv[1] if len(sys.argv)>=2 else 'day23-input.txt'
+	with open(file, 'r') as f:
+		for line in f:
+			s1, s2 = line[:-1].split('-')
+			servers[s1].add(s2)
+			servers[s2].add(s1)
+
+	res = 0
+	seen = set()
+	for s1 in servers:
+		for s2 in servers[s1]:
+			for s3 in servers[s2]:
+				if s3 in servers[s1]:
+					t = tuple(sorted((s1, s2, s3)))
+					if t not in seen:
+						seen.add(t)
+						for s in t:
+							if s.startswith('t'):
+								res += 1
+								break
+
+	print(f"Res: {res}")
+
+if __name__ == "__main__":
+    exit(main())
